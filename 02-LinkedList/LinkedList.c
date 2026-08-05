bb061e4064059edb47855fec76ba3c45cc604f02
joaocerri <joaocerri06@gmail.com>
2026-08-05 18:03:07 -0300

    feat: adicionando estrutura basica de lista
    2026-08-05
    
    feat: adicionando estrutura basica de lista

---
+++ 02-LinkedList/LinkedList.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct List{
+    int Value;
+    struct List *prox;
+}List;
+
+List* insertEnd(List *start, int value){
+    List* aux = (List*) malloc(sizeof(List));
+
+    aux -> Value = value;
+    aux -> prox = NULL;
+
+    if (start == NULL) {
+        return aux;
+    }
+
+    List* current = start;
+    while(current -> prox != NULL){
+        current = current -> prox;
+    }
+    current -> prox = aux;
+
+    return start;
+}
+
+List* insertFirst(List *start, int value){
+    List* list = (List*) malloc(sizeof(List));
+
+    list -> Value = value;
+    list -> prox = start;
+
+    return list;
+}
+
+void printList(List *start){
+    List* current = start;
+    int counter = 1;
+
+    if (current == NULL) {
+        printf("\nThe list is empty.");
+        return;
+    }
+
+    while(current != NULL){
+        printf("\nValue in this position %d: %d", counter, current -> Value);
+        current = current -> prox;
+        counter++;
+    }
+}
+
+int main()
+{
+    List *list = NULL;
+
+    list = insertFirst(list, 20);
+    list = insertFirst(list, 30);
+    list = insertEnd(list, 60);
+    list = insertEnd(list, 100);
+    list = insertFirst(list, 99);
+
+    printList(list);
+
+    return 0;
+}
+
+
